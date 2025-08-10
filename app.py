af042af49ee3ccf666f88568f7d24be52a322912
prasads6507 <prasads6507@gmail.com>
2025-08-09 22:46:28 -0400

    Updates In the App.py

--- app.py
+++ app.py
@@ -1500,11 +1500,7 @@ def delete_user(username):
 # ----------------------
 # Bootstrap DB + run
 # ----------------------
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=True)
-
-
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
